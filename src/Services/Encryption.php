341c5deda71b671189c9c76b369fb1e7bb25c100
Geoffroy <geoffroy.stolaric@maximo.fr>
2022-10-20 09:16:03 +0200

    parame decrypt true or false

--- src/Services/Encryption.php
+++ src/Services/Encryption.php
@@ -18,13 +18,13 @@ Class Encryption
 
     public function decrypt($data)
     {
-
-        $key = hash('sha256', $this->params["encryption_params"]['password']);
-        $data = base64_decode($data);
-        $ivSize = openssl_cipher_iv_length($this->params["encryption_params"]['method']);
-        $iv = substr($data, 0, $ivSize);
-        $data = openssl_decrypt(substr($data, $ivSize), $this->params["encryption_params"]['method'], $key, OPENSSL_RAW_DATA, $iv);
-
+        if($this->params["encryption_params"]['decrypt']) {
+            $key = hash('sha256', $this->params["encryption_params"]['password']);
+            $data = base64_decode($data);
+            $ivSize = openssl_cipher_iv_length($this->params["encryption_params"]['method']);
+            $iv = substr($data, 0, $ivSize);
+            $data = openssl_decrypt(substr($data, $ivSize), $this->params["encryption_params"]['method'], $key, OPENSSL_RAW_DATA, $iv);
+        }
         return $data;
     }
 
